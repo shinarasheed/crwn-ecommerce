02fd9164a65e5ddfaa19531c0ed869adfb6acb07
shina <rasheedshinaopeyemi@gmail.com>
2020-11-03 07:44:02 +0100

    started redux implementation

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,8 +3,8 @@ import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
-    <div className='group'>
-      <input className='form-input' onChange={handleChange} {...otherProps} />
+    <div className="group">
+      <input className="form-input" onChange={handleChange} {...otherProps} />
       {label ? (
         <label
           className={`${
