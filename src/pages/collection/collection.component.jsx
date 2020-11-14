4f546132b22a57892864fdd1f585de45233880d1
shina <rasheedshinaopeyemi@gmail.com>
2020-11-14 23:44:47 +0100

    implemented data normalization

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// import CollectionItem from '../../components/collection-item/collection-item.component';
-
+import CollectionItem from '../../components/collection-item/collection-item.component';
 import { selectCollection } from '../../components/redux/shop/shop.selector';
 
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
-  return <div className="collection-page">Collection page</div>;
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 //the second optional parameter in mapStateToProps is ownProps
 //it is the props of the component
 const mapStateToProps = (state, ownProps) => ({
+  //why did we include state
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
