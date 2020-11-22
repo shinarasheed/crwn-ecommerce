7014ceb85f2b32b7af038d495716d358f65b775d
shina <rasheedshinaopeyemi@gmail.com>
2020-11-22 11:43:32 +0100

    pulled down firestore data from firestore into the application

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import './custom-button.styles.scss';
 
-import { CustomButtonContainer } from './custom-button.styles';
-
-const CustomButtonm = ({ children, ...props }) => {
-  return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
+const CustomButtonm = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
+  return (
+    <button
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''
+      } custom-button`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default CustomButtonm;
+
+// import React from 'react';
+
+// import { CustomButtonContainer } from './custom-button.styles';
+
+// const CustomButtonm = ({ children, ...props }) => {
+//   return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
+// };
+
+// export default CustomButtonm;
