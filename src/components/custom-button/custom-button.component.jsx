4eb30267f640bcd7a795a3ec866d3b9f9ecd5724
shina <rasheedshinaopeyemi@gmail.com>
2020-11-22 09:24:43 +0100

    extended styled components to the custom button component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButtonm = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => {
-  return (
-    <button
-      className={`${inverted ? 'inverted' : ''} ${
-        isGoogleSignIn ? 'google-sign-in' : ''
-      } custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+import { CustomButtonContainer } from './custom-button.styles';
+
+const CustomButtonm = ({ children, ...props }) => {
+  return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
 };
 
 export default CustomButtonm;
